9e1d9d7de450265783e89ad222a0b62483a86e44
moronali09 <jonibou04@gmail.com>
2025-07-11 23:26:09 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 const mcDataLoader = require('minecraft-data');
 const logger       = require('./logger');
 
-const HOST     = 'shadow_elites.ignorelist.com';
-const PORT     = 25604;
+const HOST = 'herobrine.org';
+const PORT = 25565;
 const BOT_NAME = 'ELV';
 const VERSION  = '1.21.1';
 const OWNER    = 'moronali';
