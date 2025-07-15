538dc0b5a92635c450bea81ac725a57fbbdc0971
moronali09 <jonibou04@gmail.com>
2025-07-16 00:40:59 +0600

    Delete logger.js

--- logger.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  logJoin: (username) => {
-    console.log(`[32mJoined:[0m ${username}`);
-  },
-  logLeave: (username) => {
-    console.log(`[31mLeft:  [0m ${username}`);
-  }
-};
