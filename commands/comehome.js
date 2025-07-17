08f10367c95740aa2699e9a64819526c9746da4a
moronali09 <jonibou04@gmail.com>
2025-07-17 12:22:12 +0600

    Create comehome.js

---
+++ commands/comehome.js
@@ -0,0 +1,13 @@
+const { goals } = require('mineflayer-pathfinder');
+
+module.exports = {
+  name: '',
+  execute(bot) {
+    if (!bot.homePosition) {
+      return bot.chat('Home not set yet. Use /sethome first.');
+    }
+    const { x, y, z } = bot.homePosition;
+    bot.chat(`↩️ Coming home to X:${x.toFixed(1)}, Y:${y.toFixed(1)}, Z:${z.toFixed(1)}`);
+    bot.pathfinder.setGoal(new goals.GoalBlock(x, y, z), true);
+  }
+};
