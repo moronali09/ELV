e47315fbb09427220b304c1881ead05d130066ee
moronali09 <jonibou04@gmail.com>
2025-07-17 12:24:41 +0600

    Create dance.js

---
+++ commands/dance.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: '',
+  execute(bot) {
+    bot.chat('💃 Let's dance!');
+    let step = 0;
+    const actions = ['forward', 'back', 'left', 'right'];
+    const interval = setInterval(() => {
+      const act = actions[step % actions.length];
+      bot.setControlState(act, true);
+      setTimeout(() => bot.setControlState(act, false), 300);
+      step++;
+      if (step > 16) {
+        clearInterval(interval);
+        bot.chat('🕺 Done dancing!');
+      }
+    }, 400);
+  }
+};
