c3b99e8accbe09011565969e626e48d542a115f3
moronali09 <jonibou04@gmail.com>
2025-07-17 12:52:46 +0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,6 @@ module.exports = {
   name: 'help',
   desc: 'Show this help message',
   execute(bot) {
-    // Dynamic help from loaded commands
     bot.chat('🛠️ Available commands:');
     for (const cmd of bot.commands.values()) {
       bot.chat(`/${cmd.name}`);
